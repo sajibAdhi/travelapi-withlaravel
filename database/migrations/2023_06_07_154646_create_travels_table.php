55d44c751f1004bd94c8351d4d9aefbf9cedd93b
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-07 22:29:02 +0600

    init commit

---
+++ database/migrations/2023_06_07_154646_create_travels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_public');
+            $table->string('slug');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('number_of_days');
+            $table->integer('number_of_nights');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travels');
+    }
+};
---
+++ database/migrations/2023_06_07_154649_create_tours_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('travel_id');
+            $table->string('name');
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->integer('price');
+            $table->foreignId('travel_id')->constrained('travels')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tours');
+    }
+};
