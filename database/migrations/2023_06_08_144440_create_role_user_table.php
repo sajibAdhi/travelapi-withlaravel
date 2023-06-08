7c307b41e2ec38e5bc5e4b9af76348d335b86897
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-08 20:54:04 +0600

    UserRoleTable update

--- database/migrations/2023_06_08_144440_create_role_user_table.php
+++ database/migrations/2023_06_08_144440_create_role_user_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_role_relations', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        Schema::create('role_user', function (Blueprint $table) {
             $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
-            $table->unique(['user_id', 'role_id']);
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->primary(['role_id', 'user_id']);
         });
     }
 
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_role_relations');
+        Schema::dropIfExists('role_user');
     }
 };
--- database/migrations/2023_06_08_145251_create_tours_table.php
+++ database/migrations/2023_06_08_145251_create_tours_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('tours', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('travel_id');
             $table->string('name');
             $table->date('starting_date');
             $table->date('ending_date');
