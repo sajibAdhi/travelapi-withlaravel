29797a1adda9b61a6cbfe0674946c0cb2db96626
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-12 14:16:46 +0600

    fix: database/seeders/DatabaseSeeder.php
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Tour::factory()
+            ->count(20)
+            ->for($travel = Travel::factory()->state(['is_public' => true])->create())
+            ->state(new Sequence(
+                ['starting_date' => now(), 'ending_date' => now()->addDays($travel->number_of_days),],
+                ['starting_date' => now()->addDays(2), 'ending_date' => now()->addDays(2 + $travel->number_of_days),]
+            ))->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
