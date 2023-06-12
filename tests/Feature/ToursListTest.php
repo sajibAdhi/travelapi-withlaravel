4318ce3d339c3fad491aaf06eed4b4f9290ca011
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-12 14:18:13 +0600

    feat: ToursListTest.php added
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ tests/Feature/ToursListTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ToursListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_tours_list_by_travel_slug_return_correct_tours(): void
+    {
+        $travel = Travel::factory()->create(['is_public' => true]);
+        $tour = Tour::factory()->create(['travel_id' => $travel->id]);
+
+        $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => $tour->id]);
+    }
+
+    public function test_tours_price_is_shown_correctly(): void
+    {
+        $travel = Travel::factory()->create(['is_public' => true]);
+        Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 123.45,
+        ]);
+
+        $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['price' => '123.45',]);
+    }
+
+    public function test_tours_list_returns_paginated_data_correctly(): void
+    {
+        $toursPerPage = 15;
+
+        $travel = Travel::factory()->create();
+        Tour::factory($toursPerPage + 1)->create(['travel_id' => $travel->id]);
+
+        $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount($toursPerPage, 'data');
+        $response->assertJsonPath('meta.current_page', 1);
+    }
+}
