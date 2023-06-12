01cd9cb0823a556f6019dd43071f55414c2e0173
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-12 14:16:25 +0600

    feat: TourFactory.php added
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ database/factories/TourFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Travel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tour>
+ */
+class TourFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text(20),
+            'starting_date' => now(),
+            'ending_date' => now()->addDays(rand(1, 10)),
+            'price' => fake()->randomFloat(2, 10, 999),
+        ];
+    }
+}
