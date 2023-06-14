b5438b1f98e4c953dbae60af6a1132112ed5b23b
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-14 14:23:27 +0600

    feat: Role.php update
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
     use HasFactory, HasUuids;
 
     protected $fillable = ['name'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
