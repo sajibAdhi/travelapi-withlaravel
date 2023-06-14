b5fe32979b08e9f847ea22bc6e1d3b8c078ce4cb
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-14 13:44:48 +0600

    feat: LoginController.php added
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     * @throws ValidationException
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        dd(Hash::check($request->password, $user->password));
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'email' => ['The provided credentials are incorrect.'],
+            ], 422);
+        }
+
+        $device = substr($request->userAgent(), 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ]);
+    }
+}
