abae5b05a715d6408805a1bb1b02b13762e5b301
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-15 16:33:07 +0600

    feat: Editor\TravelController.php added
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ app/Http/Controllers/Api/V1/Editor/TravelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Editor;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TravelRequest;
+use App\Http\Resources\TravelResource;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TravelController extends Controller
+{
+    public function update(Travel $travel, TravelRequest $request): TravelResource
+    {
+        $travel = $travel->update($request->validated());
+
+        return new TravelResource($travel);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use \App\Http\Controllers\Api\V1\Admin;
+use \App\Http\Controllers\Api\V1\Editor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,12 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('travels', [TravelController::class, 'index']);
-Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+Route::post('login', LoginController::class);
 
 Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::post('travels', [Admin\TravelController::class, 'store']);
     Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
 });
 
-Route::post('login', LoginController::class);
+Route::prefix('editor')->middleware(['auth:sanctum', 'role:editor'])->group(function () {
+    Route::put('travels/{travel}', [Editor\TravelController::class, 'update']);
+});
+
+Route::get('travels', [TravelController::class, 'index']);
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
